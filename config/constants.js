a6354cbb5a95ed9f7f46215d8bd450c6b37dbe02
lovemonna0305 <lovemonna0305@gmail.com>
2024-06-11 21:13:34 +0900

    add seed for users

---
+++ config/constants.js
@@ -0,0 +1,256 @@
+exports.teams = ['1', '2', '3', '4', '5'];
+exports.offices = ['1', '2', '3', '4', '5'];
+exports.companies = ['5*4', '3*9', '8*2', 'AI', '7*9'];
+exports.roles = ["superAdmin", "companyboss", "officeboss", "teamboss", "member"];
+const SITE_URL = process.env.SITE_URL;
+
+exports.superadmin =
+{
+    userID: 'superAdmin0303',
+    password: 'superAdmin',
+    role: 'superAdmin',
+    status: true,
+    dob: '2024-05-07T07:00:00.000Z',
+    gender: 'male',
+    name: 'Super Admin',
+    organization: '7*9',
+    department: 'Manage',
+    team: '',
+    avatar: SITE_URL + "/avatar/avatar.jpg",
+    age: 100
+};
+
+exports.users =[
+    {
+        userID: 'sgi',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'So Gyong IL',
+        organization: '3*9',
+        department: 'Manage',
+        team: '1',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+   
+    {
+        userID: 'kyh',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Kim Yu Hyon',
+        organization: '3*9',
+        department: 'Manage',
+        team: '1',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'rcg',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Lee Chae Gun',
+        organization: '3*9',
+        department: 'Manage',
+        team: '1',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'sh',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Song Hyon',
+        organization: '3*9',
+        department: 'Manage',
+        team: '1',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'jgi33',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'JangGyongIl',
+        organization: '3*9',
+        department: 'Manage',
+        team: '5',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'lgm35',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'LeeGwangMyong',
+        organization: '3*9',
+        department: 'Manage',
+        team: '5',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'psh',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'PakSinHyok',
+        organization: '3*9',
+        department: 'Manage',
+        team: '5',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'shu',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'So Hyon Ung',
+        organization: '3*9',
+        department: 'Manage',
+        team: '5',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'kki',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Kim Kyong Il',
+        organization: '3*9',
+        department: 'Manage',
+        team: '5',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    
+    // 5*4
+    {
+        userID: 'ppj',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Pak Gyong Jin',
+        organization: '5*4',
+        department: 'Manage',
+        team: '1',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'myh',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Mun Yu Hyok',
+        organization: '5*4',
+        department: 'Manage',
+        team: '1',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'kcm',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Kim Chol Min',
+        organization: '5*4',
+        department: 'Manage',
+        team: '1',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'lym',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Lyu Yong Min',
+        organization: '5*4',
+        department: 'Manage',
+        team: '1',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    
+    
+    {
+        userID: 'jmc',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Jong Myong Chol',
+        organization: '5*4',
+        department: 'Manage',
+        team: '2',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'jsb',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Jang Su Bong',
+        organization: '5*4',
+        department: 'Manage',
+        team: '2',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+    {
+        userID: 'pgj',
+        password: '123123',
+        role: 'member',
+        status: true,
+        dob: '2024-05-07T07:00:00.000Z',
+        gender: 'male',
+        name: 'Pak Gyong Jin',
+        organization: '5*4',
+        department: 'Manage',
+        team: '2',
+        avatar: SITE_URL + "/avatar/avatar.jpg",
+        age: 100
+    },
+];
+
+
+
